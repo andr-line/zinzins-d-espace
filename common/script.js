cdb88a6af37ae4d0b8881ae73a297b4e28b7c301
andr-line <83190225+andr-line@users.noreply.github.com>
2023-04-23 21:35:13 +0200

    Animated Hamburger Icon

--- common/script.js
+++ common/script.js
@@ -3,4 +3,8 @@ let hamburgerButton = document.querySelector("#hamburgerButton")
 hamburgerButton.addEventListener('click', (e) => {
     let navMenu = document.querySelector("#navMenu")
     navMenu.classList.toggle("open")
+    let hamburgerChildren = document.querySelectorAll(".line")
+    hamburgerChildren.forEach((e, i) => {
+		e.classList.toggle("open")
+    })
 })
