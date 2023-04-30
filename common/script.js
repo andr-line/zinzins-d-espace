45efdac60bb150abb79eec880e75de79a645376b
andr-line <83190225+andr-line@users.noreply.github.com>
2023-04-30 23:07:45 +0200

    Vertical cards for smartphones

--- common/script.js
+++ common/script.js
@@ -1,6 +1,10 @@
+function onLoad() {
+    vertical()
+}
+
 // Hamburger Menu
 let hamburgerButton = document.querySelector("#hamburgerButton")
-hamburgerButton.addEventListener('click', (e) => {
+hamburgerButton.addEventListener("click", (e) => {
     let navMenu = document.querySelector("#navMenu")
     navMenu.classList.toggle("open")
     let hamburgerChildren = document.querySelectorAll(".line")
@@ -9,14 +13,30 @@ hamburgerButton.addEventListener('click', (e) => {
     })
 })
 
+// Orientation (for small widths on phones)
+window.addEventListener("resize", onLoad())
+
+function vertical() {
+    // Add .vertical to all elements if width is smaller than a threshold
+    if(window.innerWidth < 25 * parseInt(document.defaultView.getComputedStyle(document.body, null).getPropertyValue('font-size'))) {
+        let elements = document.querySelectorAll("*")
+        elements.forEach((e, i) => {
+            e.classList.add("vertical")
+        })
+    }
+}
+
 // Cards
 let cards = document.querySelectorAll(".card")
 cards.forEach((e, i) => {
-    e.addEventListener('click', (ev) => {
+    e.addEventListener("click", (ev) => {
         let cards = document.querySelectorAll(".card")
         cards.forEach((elem, ind) => {
-            elem.classList.remove('active')
+            if(ind==i) {
+                elem.classList.toggle("active")
+            } else {
+                elem.classList.remove("active")
+            }
         })
-        cards[i].classList.add('active')
     })
 })
